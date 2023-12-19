141675d6124c639851bfb83aa034d1af53754cd3
Pedro Franco <mrcraftsoficcial@gmail.com>
2023-12-19 19:09:22 -0300

    first commit

---
+++ dont-touch-the-spikes/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  dont-touch-the-spikes
+//
+//  Created by Pedro Franco on 19/12/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func sceneDidLoad() {
+
+    }
+    
+}
---
+++ dont-touch-the-spikes/GameViewController.swift
@@ -0,0 +1,49 @@
+//
+//  GameViewController.swift
+//  dont-touch-the-spikes
+//
+//  Created by Pedro Franco on 19/12/23.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
+        
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
